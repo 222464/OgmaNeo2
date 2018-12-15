e5c7d1c658f221809aca5cc18d5a56b40804cec6
Eric Laukien <ericlaukien@gmail.com>
2018-12-15 11:04:19 -0500

    Tuning

--- source/neo/Actor.h
+++ source/neo/Actor.h
@@ -136,7 +136,7 @@ namespace ogmaneo {
         \brief Initialize defaults
         */
         Actor()
-        : _alpha(0.01f), _gamma(0.99f), _epsilon(0.05f)
+        : _alpha(0.01f), _gamma(0.9f), _epsilon(0.05f)
         {}
 
         /*!
