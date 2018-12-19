235ea6f9edbc256b2ac304b0483b939847d8a2d6
Eric Laukien <ericlaukien@gmail.com>
2018-12-19 10:40:22 -0500

    Cleaner init ordering

--- source/neo/Actor.cpp
+++ source/neo/Actor.cpp
@@ -328,17 +328,17 @@ void Actor::createRandom(ComputeSystem &cs,
         vl._actionWeights = FloatBuffer(weightsSizeAction);
 
 #ifdef KERNEL_DEBUG
-        for (int x = 0; x < weightsSizeAction; x++)
-            init(x, cs._rng, vli);
-#else
-        runKernel1(cs, std::bind(Actor::initKernel, std::placeholders::_1, std::placeholders::_2, this, vli), weightsSizeAction, cs._rng, cs._batchSize1);
-#endif
-
-#ifdef KERNEL_DEBUG
         for (int x = 0; x < weightsSizeValue; x++)
             fillFloat(x, cs._rng, &vl._valueWeights, 0.0f);
 #else
         runKernel1(cs, std::bind(fillFloat, std::placeholders::_1, std::placeholders::_2, &vl._valueWeights, 0.0f), weightsSizeValue, cs._rng, cs._batchSize1);
+#endif
+
+#ifdef KERNEL_DEBUG
+        for (int x = 0; x < weightsSizeAction; x++)
+            init(x, cs._rng, vli);
+#else
+        runKernel1(cs, std::bind(Actor::initKernel, std::placeholders::_1, std::placeholders::_2, this, vli), weightsSizeAction, cs._rng, cs._batchSize1);
 #endif
     }
 
