5168147bebe32c820c1af08fe18bf69b190e399f
Eric Laukien <ericlaukien@gmail.com>
2019-03-05 16:50:41 -0500

    Fixes

--- source/SparseCoder.cpp
+++ source/SparseCoder.cpp
@@ -60,7 +60,7 @@ void SparseCoder::learnWeights(
 
         float sum = vl._weights.multiplyOHVsT(_hiddenCs, visibleIndex, _hiddenSize.z) / std::max(1, vl._visibleCounts[visibleColumnIndex]);
 
-        float delta = _alpha * (target - sum);
+        float delta = _alpha * (target - std::min(1.0f, std::max(0.0f, sum)));
 
         vl._weights.deltaOHVsT(_hiddenCs, delta, visibleIndex, _hiddenSize.z);
     }
