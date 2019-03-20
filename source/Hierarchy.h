94916aead22bf6fd2976099b1bf0e83a78689d8b
Eric Laukien <ericlaukien@gmail.com>
2019-03-19 20:47:27 -0400

    SARSA

--- source/Hierarchy.h
+++ source/Hierarchy.h
@@ -159,8 +159,8 @@ public:
     // Default
     Hierarchy()
     :
-    _alpha(0.01f),
-    _gamma(0.98f),
+    _alpha(0.1f),
+    _gamma(0.95f),
     _maxHistorySamples(32),
     _historyIters(8)
     {}
