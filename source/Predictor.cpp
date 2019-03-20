c203fbb243e983670cd9389b4edc1a759eda1e8f
Eric Laukien <ericlaukien@gmail.com>
2019-03-20 13:04:04 -0400

    Tuning

--- source/Predictor.cpp
+++ source/Predictor.cpp
@@ -70,8 +70,6 @@ void Predictor::learn(
             sum += vl._weights.multiplyOHVs(*inputCs[vli], hiddenIndex, vld._size.z);
         }
 
-        sum /= std::max(1, _hiddenCounts[hiddenColumnIndex]);
-
         if (sum > maxActivation) {
             maxActivation = sum;
 
