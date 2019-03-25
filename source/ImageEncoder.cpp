9db1fa00ceb77e2bc9c1bbd27b2a05031572a130
Eric Laukien <ericlaukien@gmail.com>
2019-03-25 13:01:03 -0400

    Fixes

--- source/ImageEncoder.cpp
+++ source/ImageEncoder.cpp
@@ -60,7 +60,7 @@ void ImageEncoder::forward(
                 vl._weights.hebb(*inputActivations[vli], hiddenIndex, rate);
             }
 
-            _hiddenRates[hiddenIndex] -= (1.0f - (hc == maxIndex ? _beta : 1.0f)) * _hiddenRates[hiddenIndex];
+            _hiddenRates[hiddenIndex] -= (1.0f - _beta) * strength * _hiddenRates[hiddenIndex];
         }
     }
 }
