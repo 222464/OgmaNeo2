09ea66fb73bd9aa08b604659ba2734cc6f0f8ee5
Eric Laukien <ericlaukien@gmail.com>
2019-03-24 22:24:14 -0400

    No Boltzmann

--- source/Actor.cpp
+++ source/Actor.cpp
@@ -339,7 +339,7 @@ void Actor::step(
 #ifdef KERNEL_NOTHREAD
         for (int x = 0; x < _hiddenSize.x; x++)
             for (int y = 0; y < _hiddenSize.y; y++)
-                learn(Int2(x, y), cs._rng, constGet(sPrev._inputCs), &sPrev._hiddenCs, &sPrev._hiddenValues, q, g);
+                learn(Int2(x, y), cs._rng, constGet(sPrev._inputCs), &s._hiddenCs, &sPrev._hiddenValues, q, g);
 #else
         runKernel2(cs, std::bind(Actor::learnKernel, std::placeholders::_1, std::placeholders::_2, this, constGet(sPrev._inputCs), &s._hiddenCs, &sPrev._hiddenValues, q, g), Int2(_hiddenSize.x, _hiddenSize.y), cs._rng, cs._batchSize2);
 #endif
