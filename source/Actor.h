17ae58d5eec4deea7389a902415ae68dfc7583c1
Eric Laukien <ericlaukien@gmail.com>
2019-04-14 23:46:42 -0400

    Plain Hebb

--- source/Actor.h
+++ source/Actor.h
@@ -105,9 +105,9 @@ public:
     // Defaults
     Actor()
     :
-    _alpha(0.05f),
-    _gamma(0.95f),
-    _epsilon(0.02f)
+    _alpha(0.01f),
+    _gamma(0.98f),
+    _epsilon(0.01f)
     {}
 
     Actor(
