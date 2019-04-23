46a459b9b187ff9ebc236f69f4570c8b8341dc02
Eric Laukien <ericlaukien@gmail.com>
2019-04-23 16:40:42 -0400

    Tuning

--- source/Actor.h
+++ source/Actor.h
@@ -109,9 +109,9 @@ public:
     // Defaults
     Actor()
     :
-    _alpha(0.05f),
-    _beta(1.0f),
-    _gamma(0.95f)
+    _alpha(0.01f),
+    _beta(0.5f),
+    _gamma(0.98f)
     {}
 
     Actor(
