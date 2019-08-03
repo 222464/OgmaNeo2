e16f9ccabcf1abab1835c65cba2fe434888d8b6e
Eric Laukien <ericlaukien@gmail.com>
2019-08-03 15:43:15 -0400

    Tuning

--- source/Hierarchy.h
+++ source/Hierarchy.h
@@ -161,10 +161,10 @@ public:
     Hierarchy()
     :
     _alpha(0.01f),
-    _gamma(0.99f),
-    _clip(0.1f),
+    _gamma(0.98f),
+    _clip(1.0f),
     _maxHistorySamples(32),
-    _historyIters(4)
+    _historyIters(3)
     {}
 
     // Copy
