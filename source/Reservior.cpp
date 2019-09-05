b3705ef78a5ef4ca8b127d17527ddef261941bdb
Eric Laukien <ericlaukien@gmail.com>
2019-09-05 09:10:02 -0400

    Added missing

---
+++ source/Reservior.cpp
@@ -0,0 +1,153 @@
+// ----------------------------------------------------------------------------
+//  OgmaNeo
+//  Copyright(c) 2016-2018 Ogma Intelligent Systems Corp. All rights reserved.
+//
+//  This copy of OgmaNeo is licensed to you under the terms described
+//  in the OGMANEO_LICENSE.md file included in this distribution.
+// ----------------------------------------------------------------------------
+
+#include "Reservior.h"
+
+using namespace ogmaneo;
+
+void Reservior::forward(
+    const Int2 &pos,
+    std::mt19937 &rng,
+    const std::vector<const FloatBuffer*> &inputStates
+) {
+    int hiddenColumnIndex = address2(pos, Int2(_hiddenSize.x, _hiddenSize.y));
+
+    for (int hc = 0; hc < _hiddenSize.z; hc++) {
+        int hiddenIndex = address3(Int3(pos.x, pos.y, hc), _hiddenSize);
+
+        float sum = 0.0f;
+        int count = 0;
+
+        // For each visible layer
+        for (int vli = 0; vli < _visibleLayers.size(); vli++) {
+            VisibleLayer &vl = _visibleLayers[vli];
+
+            sum += vl._weights.multiply(*inputStates[vli], hiddenIndex);
+            count += vl._weights.counts(hiddenIndex);
+        }
+
+        _hiddenStates[hiddenIndex] = std::tanh(sum / std::max(1, count));
+    }
+}
+
+void Reservior::initRandom(
+    ComputeSystem &cs,
+    const Int3 &hiddenSize,
+    const std::vector<VisibleLayerDesc> &visibleLayerDescs
+) {
+    _visibleLayerDescs = visibleLayerDescs;
+
+    _hiddenSize = hiddenSize;
+
+    _visibleLayers.resize(_visibleLayerDescs.size());
+
+    // Pre-compute dimensions
+    int numHiddenColumns = _hiddenSize.x * _hiddenSize.y;
+    int numHidden = numHiddenColumns * _hiddenSize.z;
+    
+    // Create layers
+    for (int vli = 0; vli < _visibleLayers.size(); vli++) {
+        VisibleLayer &vl = _visibleLayers[vli];
+        VisibleLayerDesc &vld = _visibleLayerDescs[vli];
+
+        int numVisibleColumns = vld._size.x * vld._size.y;
+        int numVisible = numVisibleColumns * vld._size.z;
+
+        // Create weight matrix for this visible layer and initialize randomly
+        initSMLocalRF(vld._size, _hiddenSize, vld._radius, vld._dropRatio, vl._weights, cs._rng);
+
+        std::normal_distribution<float> weightDist(0.0f, vld._scale);
+
+        for (int i = 0; i < vl._weights._nonZeroValues.size(); i++)
+            vl._weights._nonZeroValues[i] = weightDist(cs._rng);
+    }
+
+    // Hidden Cs
+    _hiddenStates = FloatBuffer(numHidden, 0.0f);
+    _hiddenStatesPrev = FloatBuffer(numHidden, 0.0f);
+}
+
+void Reservior::step(
+    ComputeSystem &cs,
+    const std::vector<const FloatBuffer*> &inputStates
+) {
+    int numHiddenColumns = _hiddenSize.x * _hiddenSize.y;
+    int numHidden = numHiddenColumns * _hiddenSize.z;
+
+    // Copy to prev
+#ifdef KERNEL_NOTHREAD
+    for (int x = 0; x < numHidden; x++)
+        copyFloat(x, cs._rng, &_hiddenStates, &_hiddenStatesPrev);
+#else
+    runKernel1(cs, std::bind(copyFloat, std::placeholders::_1, std::placeholders::_2, &_hiddenStates, &_hiddenStatesPrev), numHidden, cs._rng, cs._batchSize1);
+#endif
+
+#ifdef KERNEL_NOTHREAD
+    for (int x = 0; x < _hiddenSize.x; x++)
+        for (int y = 0; y < _hiddenSize.y; y++)
+            forward(Int2(x, y), cs._rng, inputStates);
+#else
+    runKernel2(cs, std::bind(Reservior::forwardKernel, std::placeholders::_1, std::placeholders::_2, this, inputStates), Int2(_hiddenSize.x, _hiddenSize.y), cs._rng, cs._batchSize2);
+#endif
+}
+
+void Reservior::writeToStream(
+    std::ostream &os
+) const {
+    int numHiddenColumns = _hiddenSize.x * _hiddenSize.y;
+    int numHidden = numHiddenColumns * _hiddenSize.z;
+
+    os.write(reinterpret_cast<const char*>(&_hiddenSize), sizeof(Int3));
+
+    writeBufferToStream(os, &_hiddenStates);
+    writeBufferToStream(os, &_hiddenStatesPrev);
+
+    int numVisibleLayers = _visibleLayers.size();
+
+    os.write(reinterpret_cast<char*>(&numVisibleLayers), sizeof(int));
+    
+    for (int vli = 0; vli < _visibleLayers.size(); vli++) {
+        const VisibleLayer &vl = _visibleLayers[vli];
+        const VisibleLayerDesc &vld = _visibleLayerDescs[vli];
+
+        os.write(reinterpret_cast<const char*>(&vld), sizeof(VisibleLayerDesc));
+
+        writeSMToStream(os, vl._weights);
+    }
+}
+
+void Reservior::readFromStream(
+    std::istream &is
+) {
+    is.read(reinterpret_cast<char*>(&_hiddenSize), sizeof(Int3));
+
+    int numHiddenColumns = _hiddenSize.x * _hiddenSize.y;
+    int numHidden = numHiddenColumns * _hiddenSize.z;
+
+    readBufferFromStream(is, &_hiddenStates);
+    readBufferFromStream(is, &_hiddenStatesPrev);
+
+    int numVisibleLayers;
+    
+    is.read(reinterpret_cast<char*>(&numVisibleLayers), sizeof(int));
+
+    _visibleLayers.resize(numVisibleLayers);
+    _visibleLayerDescs.resize(numVisibleLayers);
+    
+    for (int vli = 0; vli < _visibleLayers.size(); vli++) {
+        VisibleLayer &vl = _visibleLayers[vli];
+        VisibleLayerDesc &vld = _visibleLayerDescs[vli];
+
+        is.read(reinterpret_cast<char*>(&vld), sizeof(VisibleLayerDesc));
+
+        int numVisibleColumns = vld._size.x * vld._size.y;
+        int numVisible = numVisibleColumns * vld._size.z;
+
+        readSMFromStream(is, vl._weights);
+    }
+}
---
+++ source/Reservior.h
@@ -0,0 +1,135 @@
+// ----------------------------------------------------------------------------
+//  OgmaNeo
+//  Copyright(c) 2016-2018 Ogma Intelligent Systems Corp. All rights reserved.
+//
+//  This copy of OgmaNeo is licensed to you under the terms described
+//  in the OGMANEO_LICENSE.md file included in this distribution.
+// ----------------------------------------------------------------------------
+
+#pragma once
+
+#include "ComputeSystem.h"
+
+namespace ogmaneo {
+// Sparse coder
+class Reservior {
+public:
+    // Visible layer descriptor
+    struct VisibleLayerDesc {
+        Int3 _size; // Size of input
+
+        int _radius; // Radius onto input
+
+        float _scale; // Scale of weights
+        float _dropRatio; // Ratio of weights to drop
+
+        // Defaults
+        VisibleLayerDesc()
+        :
+        _size(4, 4, 16),
+        _radius(2),
+        _scale(1.0f),
+        _dropRatio(0.0f)
+        {}
+    };
+
+    // Visible layer
+    struct VisibleLayer {
+        SparseMatrix _weights; // Weight matrix
+    };
+
+private:
+    Int3 _hiddenSize; // Size of hidden/output layer
+
+    FloatBuffer _hiddenStates; // Hidden states
+    FloatBuffer _hiddenStatesPrev; // Previous tick hidden states
+
+    // Visible layers and associated descriptors
+    std::vector<VisibleLayer> _visibleLayers;
+    std::vector<VisibleLayerDesc> _visibleLayerDescs;
+    
+    // --- Kernels ---
+    
+    void forward(
+        const Int2 &pos,
+        std::mt19937 &rng,
+        const std::vector<const FloatBuffer*> &inputStates
+    );
+
+    static void forwardKernel(
+        const Int2 &pos,
+        std::mt19937 &rng,
+        Reservior* sc,
+        const std::vector<const FloatBuffer*> &inputStates
+    ) {
+        sc->forward(pos, rng, inputStates);
+    }
+
+public:
+    Reservior() {}
+
+    // Create a sparse coding layer with random initialization
+    void initRandom(
+        ComputeSystem &cs, // Compute system
+        const Int3 &hiddenSize, // Hidden/output size
+        const std::vector<VisibleLayerDesc> &visibleLayerDescs // Descriptors for visible layers
+    );
+
+    // Activate the sparse coder (perform sparse coding)
+    void step(
+        ComputeSystem &cs, // Compute system
+        const std::vector<const FloatBuffer*> &inputStates
+    );
+
+    // Write to stream
+    void writeToStream(
+        std::ostream &os // Stream to write to
+    ) const;
+
+    // Read from stream
+    void readFromStream(
+        std::istream &is // Stream to read from
+    );
+
+    // Get the number of visible layers
+    int getNumVisibleLayers() const {
+        return _visibleLayers.size();
+    }
+
+    // Get a visible layer
+    const VisibleLayer &getVisibleLayer(
+        int i // Index of visible layer
+    ) const {
+        return _visibleLayers[i];
+    }
+
+    // Get a visible layer descriptor
+    const VisibleLayerDesc &getVisibleLayerDesc(
+        int i // Index of visible layer
+    ) const {
+        return _visibleLayerDescs[i];
+    }
+
+    // Get the hidden states
+    const FloatBuffer &getHiddenStates() const {
+        return _hiddenStates;
+    }
+
+    // Get the hidden states
+    const FloatBuffer &getHiddenStatesPrev() const {
+        return _hiddenStatesPrev;
+    }
+
+    // Get the hidden size
+    const Int3 &getHiddenSize() const {
+        return _hiddenSize;
+    }
+
+    // Get the weights for a visible layer
+    const SparseMatrix &getWeights(
+        int i // Index of visible layer
+    ) const {
+        return _visibleLayers[i]._weights;
+    }
+};
+} // namespace ogmaneo
