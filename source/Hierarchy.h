c19069c82a505d293f18927a074a8fc4e600440a
Eric Laukien <ericlaukien@gmail.com>
2019-09-20 09:25:50 -0400

    Fixes

--- source/Hierarchy.h
+++ source/Hierarchy.h
@@ -162,8 +162,8 @@ public:
     // Default
     Hierarchy()
     :
-    _alpha(0.001f),
-    _beta(0.01f),
+    _alpha(0.01f),
+    _beta(0.1f),
     _gamma(0.99f),
     _maxHistorySamples(32),
     _historyIters(4)
