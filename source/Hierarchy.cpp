d99cdcecbd127e4a1713c6ea0636a6d1393877e3
Eric Laukien <ericlaukien@gmail.com>
2019-09-22 11:09:35 -0400

    Fixes

--- source/Hierarchy.cpp
+++ source/Hierarchy.cpp
@@ -81,7 +81,7 @@ void Hierarchy::forward(
 
         sum /= std::max(1, _rLayers[l]._visibleCounts[vli][visibleColumnIndex]);
     
-        _rLayers[l]._activations[vli][visibleColumnIndex] = sum;//std::min(1.0f, std::max(0.0f, sum));
+        _rLayers[l]._activations[vli][visibleColumnIndex] = std::min(1.0f, std::max(0.0f, sum));
     }
 }
 
@@ -107,18 +107,18 @@ void Hierarchy::backward(
 
         error /= std::max(1, _rLayers[l]._hiddenCounts[hiddenColumnIndex]);
 
-        //float act = _rLayers[l + 1]._activations[0][hiddenColumnIndex];
+        float act = _rLayers[l + 1]._activations[0][hiddenColumnIndex];
 
-        _rLayers[l + 1]._errors[0][hiddenColumnIndex] = error;//(act > 0.0f && act < 1.0f ? error : 0.0f);
+        _rLayers[l + 1]._errors[0][hiddenColumnIndex] = (act > 0.0f && act < 1.0f ? error : 0.0f);
     }
     else {
         float error = _rLayers[l]._weights[0].multiplyOHVsT(*inputCs[0], _rLayers[l]._errors[0], hiddenIndex, _scLayers[l].getHiddenSize().z);
 
         error /= std::max(1, _rLayers[l]._hiddenCounts[hiddenColumnIndex]);
 
-        //float act = _rLayers[l + 1]._activations[0][hiddenColumnIndex];
+        float act = _rLayers[l + 1]._activations[0][hiddenColumnIndex];
 
-        _rLayers[l + 1]._errors[0][hiddenColumnIndex] = error;//(act > 0.0f && act < 1.0f ? error : 0.0f);
+        _rLayers[l + 1]._errors[0][hiddenColumnIndex] = (act > 0.0f && act < 1.0f ? error : 0.0f);
     }
 }
 
@@ -189,7 +189,7 @@ void Hierarchy::initRandom(
 
     // Iterate through layers
     for (int l = 0; l < layerDescs.size(); l++) {
-        std::uniform_real_distribution<float> weightDist(0.99f, 1.01f);
+        std::uniform_real_distribution<float> weightDist(0.99f, 1.0f);
 
         // Histories for all input layers or just the one sparse coder (if not the first layer)
         _histories[l].resize(l == 0 ? inputSizes.size() * layerDescs[l]._temporalHorizon : layerDescs[l]._temporalHorizon);
