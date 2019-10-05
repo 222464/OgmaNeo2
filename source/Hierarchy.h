94bfddbd721a645fdd4413931a09007bd8d484a3
Eric Laukien <ericlaukien@gmail.com>
2019-10-05 11:06:40 -0400

    Elu

--- source/Hierarchy.h
+++ source/Hierarchy.h
@@ -163,8 +163,8 @@ public:
     _alpha(0.01f),
     _beta(0.01f),
     _gamma(0.99f),
-    _maxHistorySamples(32),
-    _historyIters(4)
+    _maxHistorySamples(64),
+    _historyIters(8)
     {}
 
     // Copy
