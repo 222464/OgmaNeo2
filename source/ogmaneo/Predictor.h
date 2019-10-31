284f273c460f90ca927724ecb2ee4c00bcb61086
Eric Laukien <ericlaukien@gmail.com>
2019-10-31 19:52:34 -0400

    Impl

--- source/ogmaneo/Predictor.h
+++ source/ogmaneo/Predictor.h
@@ -95,8 +95,8 @@ public:
     // Defaults
     Predictor()
     :
-    _alpha(0.05f),
-    _gamma(0.98f),
+    _alpha(0.02f),
+    _gamma(0.9f),
     _maxHistorySize(64),
     _historyIters(8)
     {}
