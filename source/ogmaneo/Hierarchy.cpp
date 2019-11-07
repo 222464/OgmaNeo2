3f99cdb6a0f73a8d92583678ae828ef2a36c836b
Eric Laukien <ericlaukien@gmail.com>
2019-11-07 10:30:30 -0500

    Tests

--- source/ogmaneo/Hierarchy.cpp
+++ source/ogmaneo/Hierarchy.cpp
@@ -181,8 +181,8 @@ void Hierarchy::initRandom(
 
     // Iterate through layers
     for (int l = 0; l < layerDescs.size(); l++) {
-        const float lowMul = 0.01f;
-        std::uniform_real_distribution<float> weightDist(0.99f, 1.0f);
+        std::uniform_real_distribution<float> weightDistLow(-0.01f, 0.01f);
+        std::uniform_real_distribution<float> weightDistHigh(0.99f, 1.0f);
 
         // Histories for all input layers or just the one sparse coder (if not the first layer)
         _histories[l].resize(l == 0 ? inputSizes.size() * layerDescs[l]._temporalHorizon : layerDescs[l]._temporalHorizon);
@@ -225,7 +225,7 @@ void Hierarchy::initRandom(
 
                     // Init weights
                     for (int j = 0; j < _rLayers[l]._weights[i]._nonZeroValues.size(); j++)
-                        _rLayers[l]._weights[i]._nonZeroValues[j] = weightDist(cs._rng) * lowMul;
+                        _rLayers[l]._weights[i]._nonZeroValues[j] = weightDistLow(cs._rng);
 
                     for (int x = 0; x < layerDescs[l]._hiddenSize.x; x++)
                         for (int y = 0; y < layerDescs[l]._hiddenSize.y; y++) {
@@ -283,7 +283,7 @@ void Hierarchy::initRandom(
 
             // Init weights
             for (int j = 0; j < _rLayers[l]._weights[0]._nonZeroValues.size(); j++)
-                _rLayers[l]._weights[0]._nonZeroValues[j] = weightDist(cs._rng);
+                _rLayers[l]._weights[0]._nonZeroValues[j] = weightDistHigh(cs._rng);
 
             for (int x = 0; x < layerDescs[l]._hiddenSize.x; x++)
                 for (int y = 0; y < layerDescs[l]._hiddenSize.y; y++) {
