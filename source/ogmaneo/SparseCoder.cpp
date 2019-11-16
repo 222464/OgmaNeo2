95db36860e29ad62ed4e410fe5412d1d7f3c8f65
Eric Laukien <ericlaukien@gmail.com>
2019-11-16 09:39:17 -0500

    Tests

--- source/ogmaneo/SparseCoder.cpp
+++ source/ogmaneo/SparseCoder.cpp
@@ -77,7 +77,7 @@ void SparseCoder::learn(
         for (int vc = 0; vc < vld._size.z; vc++) {
             int visibleIndex = address3(Int3(pos.x, pos.y, vc), vld._size);
 
-            float delta = _alpha * ((vc == targetC ? 1.0f : 0.0f) - std::exp(activations[vc]));
+            float delta = _alpha * ((vc == targetC ? 1.0f : 0.0f) - std::min(1.0f, std::max(0.0f, activations[vc])));
 
             vl._weights.deltaOHVsT(_hiddenCs, delta, visibleIndex, _hiddenSize.z);
         }
@@ -99,7 +99,7 @@ void SparseCoder::initRandom(
     int numHiddenColumns = _hiddenSize.x * _hiddenSize.y;
     int numHidden = numHiddenColumns * _hiddenSize.z;
 
-    std::uniform_real_distribution<float> weightDist(-0.01f, 0.0f);
+    std::uniform_real_distribution<float> weightDist(0.99f, 1.0f);
 
     // Create layers
     for (int vli = 0; vli < _visibleLayers.size(); vli++) {
