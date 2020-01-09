e5ab46b676c511105eb06569534b3cf69e3f64c1
Eric Laukien <ericlaukien@gmail.com>
2020-01-09 12:06:08 -0500

    Deterministic

--- source/ogmaneo/Hierarchy.cpp
+++ source/ogmaneo/Hierarchy.cpp
@@ -132,7 +132,7 @@ void Hierarchy::learn(
 
         int visibleIndex = address3(Int3(pos.x, pos.y, (*inputCs)[visibleColumnIndex]), _inputSizes[vli]);
 
-        float delta = _alpha * std::tanh(_rLayers[l]._errors[vli][visibleColumnIndex]);
+        float delta = _alpha * _rLayers[l]._errors[vli][visibleColumnIndex];
         
         if (l == _scLayers.size() - 1)
             _rLayers[l]._weights[vli].deltaOHVs(*hiddenCs, delta, visibleIndex, _scLayers[l].getHiddenSize().z);
@@ -144,7 +144,7 @@ void Hierarchy::learn(
 
         int visibleIndex = address3(Int3(pos.x, pos.y, (*inputCs)[visibleColumnIndex]), _scLayers[l - 1].getHiddenSize());
 
-        float delta = _beta * std::tanh(_rLayers[l]._errors[vli][visibleColumnIndex]);
+        float delta = _beta * _rLayers[l]._errors[vli][visibleColumnIndex];
         
         if (l == _scLayers.size() - 1)
             _rLayers[l]._weights[vli].deltaOHVs(*hiddenCs, delta, visibleIndex, _scLayers[l].getHiddenSize().z);
