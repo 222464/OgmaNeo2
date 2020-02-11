74c8d295a43b8cb321d7772219f1922e4bd0acca
Eric Laukien <ericlaukien@gmail.com>
2020-02-11 10:14:32 -0500

    Usage is back

--- source/ogmaneo/SparseCoder.cpp
+++ source/ogmaneo/SparseCoder.cpp
@@ -63,7 +63,7 @@ void SparseCoder::inhibit(
 
         float sum = laterals.multiplyNoDiagonalOHVs(hiddenCsTemp, hiddenIndex, hiddenSize.z) / std::max(1, laterals.count(hiddenIndex) / hiddenSize.z - 1); // -1 for missing diagonal
         
-        hiddenActivations[hiddenIndex] += hiddenStimuli[hiddenIndex] - sum;
+        hiddenActivations[hiddenIndex] += std::max(0.0f, hiddenStimuli[hiddenIndex] - sum);
 
         if (hiddenActivations[hiddenIndex] > maxActivation) {
             maxActivation = hiddenActivations[hiddenIndex];
