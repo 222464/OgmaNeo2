e870d1a17dde1e29240781ed7cf3b5b49df70836
Eric Laukien <ericlaukien@gmail.com>
2020-03-14 20:38:37 -0400

    Fix

--- source/ogmaneo/Actor.cpp
+++ source/ogmaneo/Actor.cpp
@@ -243,7 +243,7 @@ void Actor::step(
     }
 
     // Learn (if have sufficient samples)
-    if (learnEnabled && historySize > 1 + qSteps) {
+    if (learnEnabled && historySize > qSteps) {
         std::uniform_int_distribution<int> sampleDist(1, historySize - qSteps);
 
         for (int it = 0; it < historyIters; it++) {
